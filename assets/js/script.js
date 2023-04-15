1394e843ac34f436071a97e58611b03b57372bbe
michael-hoang <mike081522@gmail.com>
2023-04-15 13:11:38 -0700

    Replace basic math operator functions with operators object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,14 @@
-// Basic math operations
-function add(a, b) {
-    return a + b;
+// Basic math operator functions
+const operators = {
+    '+': (a, b) => {return a + b},
+    '-': (a, b) => {return a - b},
+    '*': (a, b) => {return a * b},
+    '/': (a, b) => {return a / b},
 }
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
 // Container for the operands and the operator
 let a, operator, b;
 
+// Math operation
+function operate(a, operator, b) {
+
+}
