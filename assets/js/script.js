2ec8891b9bcb2e7b03fff67302c41c5b7fe0d464
michael-hoang <mike081522@gmail.com>
2023-04-15 16:35:52 -0700

    Add class operators to operator buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Basic math operator functions
+// Basic math operator functions inside an object
 const operators = {
     '+': (a, b) => {return a + b},
     '-': (a, b) => {return a - b},
@@ -6,19 +6,27 @@ const operators = {
     '/': (a, b) => {return a / b},
 }
 // Container for the operands and the operator
-let a, operator, b;
+let a = '';
+let operator = '';
+let b = '';
 
-// Math operation
+// Math operation function
 function operate(a, operator, b) {
     return operators[operator](a, b);
 }
 
-// Populate display with digit button press
+// Reference to HTML elements
 const digitBtns = document.querySelectorAll('.digits');
-const display = document.querySelector('#display-text')
+const operatorBtns = document.querySelectorAll('.operators');
+const display = document.querySelector('#display-text');
+
+// Event listeners
 digitBtns.forEach((btn) => {
     btn.addEventListener('click', (event) => {
         let value = event.target.innerHTML;
-        display.innerHTML = value;
+        if (operator === '') {
+            a += (value);
+            display.innerHTML = a;
+        }
     });
 });
